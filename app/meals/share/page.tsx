133c5c0fe301f1aaff708933f99e98651bbeee25
st:prchal <steveruu@stvr.cz>
2024-07-31 14:12:57 +0200

    meal creation,+ max char limit, fix slug url issues

--- app/meals/share/page.tsx
+++ app/meals/share/page.tsx
@@ -1,5 +1,7 @@
 import classes from './page.module.css';
 import ImagePicker from "@/components/Meals/ImagePicker";
+import { shareMeal } from "@/lib/actions";
+import MealsFormSubmit from "@/components/Meals/MealsFormSubmit";
 
 export default function ShareMealPage() {
   return (
@@ -11,7 +13,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -41,7 +43,7 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker label="Meal Image" name="image" />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
--- lib/meals.ts
+++ lib/meals.ts
@@ -1,4 +1,17 @@
 import sql from "better-sqlite3";
+import slugify from "slugify";
+import xss from "xss";
+import fs from "node:fs";
+
+interface MealDB {
+    slug: string;
+    title: string;
+    image: any;
+    summary: string;
+    instructions: string;
+    creator: string;
+    creator_email: string;
+}
 
 const db = sql("meals.db");
 
@@ -10,4 +23,38 @@ export async function getMeals() {
 
 export function getMeal(slug: string) {
     return db.prepare("SELECT * FROM meals WHERE slug = ?").get(slug); // "?" brani proti SQL injections - better-sqlite3 feature
+}
+
+export async function createMeal(meal: MealDB) {
+    meal.slug = Date.now().toString().slice(0, 16) + "-" + slugify(meal.title?.toString(), { lower: true }); 
+    meal.slug = meal.slug.slice(0, 64); // maximum character limit bude prostě 64 a nasrat
+    meal.instructions = xss(meal.instructions.toString()); // sanitizace proti XSS
+    // nvm tak tohle sanitizaci ani z okna nevidělo
+    
+    meal.instructions = meal.instructions.replaceAll("<", " ").replaceAll(">", " ");
+
+
+    const extension = meal.image.name.split(".").pop();
+    const fileName = `${meal.slug.slice(0, 16)}-${Date.now()}.${extension}`; // to avoid duplicate images
+
+    const stream = fs.createWriteStream(`public/images/${fileName}`);
+    const bufferedImage = await meal.image.arrayBuffer();
+
+    stream.write(Buffer.from(bufferedImage), (error) => {
+        if (error) {
+            throw new Error("Saving image failed!");
+        }
+    });
+
+    meal.image = `/images/${fileName}`;
+
+    return db
+        .prepare(
+            `
+            INSERT INTO meals 
+                (title, summary, instructions, creator, creator_email, image, slug) 
+            VALUES (@title, @summary, @instructions, @creator, @creator_email, @image, @slug)
+        `
+        )
+        .run(meal);
 }
